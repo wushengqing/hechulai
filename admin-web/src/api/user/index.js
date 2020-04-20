51f06886d6d13d10b0c04dd16041f2b73abe4b37
wushengqing <1446412310@qq.com>
2020-04-20 22:50:39 +0800

    用户中心修改

--- admin-web/src/api/user/index.js
+++ admin-web/src/api/user/index.js
@@ -1,33 +1,54 @@
 import axios from 'axios'
 
 export default {
-  /**
-   * 用户注册
-   * @returns {*}
-   */
-  register (vo) {
-    return axios.post('/hcl-web/web/userRegistered', {
-      ...vo,
-    })
-  },
-  /**
-   * 用户登陆
-   * @returns {*}
-   */
-  login (vo) {
-    return axios.post('/hcl-web/web/userLogin', {
-      ...vo,
-    })
-  },
-  /**
-   * get接口
-   * @returns {*}
-   */
-  del (id) {
-    return axios.get('/hcl-web/web/motor/item/del', {
-      params: {
-        id
-      }
-    })
-  },
+	/**
+	 * 用户注册
+	 * @returns {*}
+	 */
+	register(vo) {
+		return axios.post('/hcl-web/web/userRegistered', {
+			...vo,
+		})
+	},
+	/**
+	 * 用户登陆
+	 * @returns {*}
+	 */
+	login(vo) {
+		return axios.post('/hcl-web/web/userLogin', {
+			...vo,
+		})
+	},
+	/**
+	 * 查询用户列表
+	 * @returns {*}
+	 */
+	list(condition, pageInfo) {
+		pageInfo = Object.assign({
+		  currentPage: 1,
+		  pageSize: 10
+		}, pageInfo)
+		return axios.post('/hcl-web/web/getUserList', {
+			...condition,
+			...pageInfo
+		})
+	},
+	/**
+	 * 编辑用户信息
+	 * @returns {*}
+	 */
+	upDateUserInfo(vo) {
+		return axios.post('/hcl-web/web/addOrUpdateUserInfo', {
+			...vo,
+		})
+	},
+	/**
+	 * 编辑用户信息
+	 * @returns {*}
+	 */
+	changeUserState(vo) {
+		return axios.post('/hcl-web/web/changeUserState', {
+			...vo,
+		})
+	},
 }
--- admin-web/src/pages/user/List/index.vue
+++ admin-web/src/pages/user/List/index.vue
@@ -1,223 +1,132 @@
 <template>
-  <div class="workapply-area">
-    <d2-container>
-      <template slot="header">菜单列表</template>
+	<div class="workapply-area">
+		<d2-container>
+			<template slot="header">菜单列表</template>
 
-      <!---->
-      <table-comb
-        name="菜单列表"
-        ref="tableMain"
-        :search-model-base="tableMainSearchModelBase"
-        :get-action="$api.union.list"
-        :get-action-where="getActionWhere"
-        :afterFetchData="afterFetchData">
-        <!--基础查询-->
-        <template slot="baseSearchForm" slot-scope="scope">
-          <el-input
-            placeholder="请输入关键字"
-            prefix-icon="el-icon-search"
-            clearable
-            v-model="scope.form.keyword"
-            style="width: 250px; margin-left: 10px; margin-right: 10px">
-          </el-input>
-          <el-button class="fr ml10" @click="addRubbish">新增</el-button>
-        </template>
-        <!--表格-->
-        <template slot="tableColumns">
-          <el-table-column
-            prop="itemName"
-            label="名称">
-          </el-table-column>
-          <el-table-column
-            prop="belongCategory.categoryName"
-            label="所在地区">
-          </el-table-column>
-          <el-table-column
-            prop="belongCategory.categoryName"
-            label="详细地址">
-          </el-table-column>
-          <el-table-column
-            prop="belongCategory.categoryName"
-            label="宗族管理员名称">
-          </el-table-column>
-          <el-table-column
-            prop="belongCategory.description"
-            label="描述">
-          </el-table-column>
-          <el-table-column label="是否启用">
-            <template slot-scope="props">
-              {{ props.row.status===1?'前台显示':'前台不显示' }}
-            </template>
-          </el-table-column>
+			<!---->
+			<table-comb name="菜单列表" ref="tableMain" :search-model-base="tableMainSearchModelBase" :get-action="$api.user.list"
+			 :get-action-where="getActionWhere" :afterFetchData="afterFetchData">
+				<!--基础查询-->
+				<template slot="baseSearchForm" slot-scope="scope">
+					<el-input placeholder="请输入关键字" prefix-icon="el-icon-search" clearable v-model="scope.form.keyword" style="width: 250px; margin-left: 10px; margin-right: 10px">
+					</el-input>
+					<!-- <el-button class="fr ml10" @click="addRubbish">新增</el-button> -->
+				</template>
+				<!--表格-->
+				<template slot="tableColumns">
+					<el-table-column prop="name" label="用户名称">
+					</el-table-column>
+					<el-table-column prop="userNum" label="登录名">
+					</el-table-column>
+					<el-table-column prop="sex" label="性别">
+						<template slot-scope="props">
+							{{ props.row.sex !==null ?
+					props.row.sex==='1'?
+					'男':
+					'女':
+					'未填写'
+			  }}
+						</template>
+					</el-table-column>
+					<el-table-column label="是否启用">
+						<template slot-scope="props">
+							{{ props.row.status===1?'已启用':'未启用' }}
+						</template>
+					</el-table-column>
 
-          <el-table-column label="操作" width="200px">
-            <template slot-scope="props">
-              <el-button type="text" size="mini" @click="editRubbish(props.row)">编辑</el-button>
-              <el-button type="text" size="mini" @click="editRubbish(props.row)">族谱管理</el-button>
-              <el-button type="text" size="mini" @click="delRubbish(props.row)">删除</el-button>
-            </template>
-          </el-table-column>
-        </template>
-      </table-comb>
-      <template slot="footer"></template>
-    </d2-container>
+					<el-table-column label="操作" width="200px">
+						<template slot-scope="props">
+							<el-button type="text" size="mini" @click="resetPassword(props.row)">重置密码</el-button>
+							<el-button type="text" size="mini" @click="editRubbish(props.row)">族谱管理</el-button>
+							<el-button v-if="props.row.status===1" type="text" size="mini" @click="changeUserState(props.row)">停用</el-button>
+							<el-button v-if="props.row.status!==1" type="text" size="mini" @click="changeUserState(props.row)">启用</el-button>
 
-    <el-dialog :title="dialogVO.id?'编辑':'新增'" :visible.sync="dialogShow">
-      <el-form ref="dialogVO" :model="dialogVO" label-width="150px" :rules="rules">
-        <el-form-item label="垃圾名称：" prop="itemName">
-          <el-input style="width: 210px" v-model="dialogVO.itemName"></el-input>
-        </el-form-item>
-        <el-form-item label="所属类型：" prop="categoryId">
-          <el-select style="width: 210px" v-model="dialogVO.categoryId" placeholder="请选择分类">
-            <el-option
-              v-for="item in categoryList"
-              :label="item.categoryName"
-              :value="item.id">
-            </el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="是否在前台显示：">
-          <el-switch v-model="dialogVO.status"></el-switch>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="save">保 存</el-button>
-        <el-button @click="dialogShow = false">取 消</el-button>
-      </div>
-    </el-dialog>
-  </div>
+						</template>
+					</el-table-column>
+				</template>
+			</table-comb>
+			<template slot="footer"></template>
+		</d2-container>
+
+
+	</div>
 </template>
 
 <script>
-  import listMixin from "@/mixins/list.mixin";
-  export default {
-    // 如果需要缓存页 name 字段需要设置为和本页路由 name 字段一致
-    name: "UnionList",
-    components: {},
-    mixins: [
-      listMixin
-    ],
-    data() {
-      return {
-        tableMainSearchModelBase:{
-          keyword: '',
-          categoryId:''
-        },
-        //分类列表
-        categoryList:[],
-        dialogShow:false,
-        //新增或者编辑的vo
-        dialogVO:{
-          id:'',
-          categoryId:'',
-          itemName:'',
-          status:true
-        },
-        rules: {
-          itemName: [
-            { required: true, message: '请输入垃圾名称'},
-          ],
-          categoryId: [
-            { required: true, message: '请选择垃圾类型'},
-          ],
-        }
-      };
-    },
-    computed: {
-      getActionWhere(){
-        return {
+	import listMixin from "@/mixins/list.mixin";
+	export default {
+		// 如果需要缓存页 name 字段需要设置为和本页路由 name 字段一致
+		name: "UnionList",
+		components: {},
+		mixins: [
+			listMixin
+		],
+		data() {
+			return {
+				tableMainSearchModelBase: {
+					keyword: '',
+					categoryId: ''
+				},
+			}
+		},
+		computed: {
+			getActionWhere() {
+				return {
 
-        }
-      },
-    },
-    methods: {
-      addRubbish(){
-        this.dialogVO= {
-          categoryId:'',
-          itemName:'',
-          status:true
-        }
-        this.openDialog();
-      },
-      editRubbish(detailVO){
-        this.dialogVO= {
-          id:detailVO.id,
-          categoryId:detailVO.belongCategory.id,
-          itemName:detailVO.itemName,
-          status:detailVO.status==1,
-        }
-        this.openDialog();
-      },
-      openDialog(){
-        if(this.$refs['dialogVO']){
-          this.$nextTick(()=>{
-            this.$refs['dialogVO'].clearValidate();
-          })
-        }
-        this.dialogShow = true;
-      },
-      delRubbish(vo){
-        this.$confirm('确认删除该记录？')
-          .then(() => {
-            this.$api.rubbish.del(vo.id).then(res=>{
-              if(res.status==1){
-                this.$message.success('删除成功！');
-                this.$refs.tableMain.fetchData();
-              }else {
-                this.$message.error(res.errorMessage)
-              }
+				}
+			}
+		},
+		methods: {
+			resetPassword(item) {
+				this.$confirm('是否需要重置密码，重置密码之后，用户的密码将重置为"123456"', '提示 ', {
+					confirmButtonText: '确定',
+					cancelButtonText: '取消',
+					type: 'warning'
+				}).then(() => {
+					this.$api.user.upDateUserInfo({
+						...item,
+						userPassword: '123456'
+					}).then(res => {
+						if (res.code === 0) {
+							this.$message.success('密码重置成功！');
+						} else {
+							this.$message.error(res.msg);
+						}
+					})
 
-            });
-          })
-          .catch(confirm => {});
-      },
-      save(){
-        this.$refs['dialogVO'].validate((valid) => {
-          if (valid) {
-            let vo = {
-              ...this.dialogVO,
-              status: this.dialogVO.status?1:0
-            };
-            //新增
-            if(!vo.id){
-              delete vo.id;
-              //
-              this.$api.rubbish.create(vo).then(res=>{
-                if(res.status==1){
-                  this.dialogShow = false
-                  this.$message.success('新增成功！');
-                  this.$refs.tableMain.fetchData();
-                }else{
-                  this.$message.error(res.errorMessage)
-                }
+				}).catch(() => {
+					this.$message.info('已取消操作');
+				});
+			},
+			changeUserState(item) {
+				let confirmText = '';
+				let status = item.status === 1 ? 0 : 1;
+				if (item.status !== '1') {
+					confirmText = '是否启用用户'
+				}
+				this.$confirm(confirmText, '提示 ', {
+					confirmButtonText: '确定',
+					cancelButtonText: '取消',
+					type: 'warning'
+				}).then(() => {
+					this.$api.user.upDateUserInfo({
+						id: item.id,
+						status
+					}).then(res => {
+						if (res.code === 0) {
+							this.$message.success('密码重置成功！');
+						} else {
+							this.$message.error(res.msg);
+						}
+					})
 
-              })
-            }else{
-              this.$api.rubbish.edit(vo).then(res=>{
-                if(res.status==1){
-                  this.dialogShow = false
-                  this.$message.success('修改成功！');
-                  this.$refs.tableMain.fetchData();
-                }else{
-                  this.$message.error(res.errorMessage)
-                }
+				}).catch(() => {
+					this.$message.info('已取消操作');
+				});
+			},
+		},
+		mounted() {
 
-              })
-            }
-          } else {
-            return false;
-          }
-        });
-      },
-      afterFetchData(){
-
-      }
-    },
-    mounted() {
-      //获取垃圾类型列表
-      this.$api.rubbish.categoryList({}).then(res=>{
-        this.categoryList = res.data;
-      })
-    }
-  };
+		}
+	};
 </script>
