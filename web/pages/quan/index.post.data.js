8f1bc680497648008aaffbe1420a3cd0621884cf
sqwu <sqwu@yuanqubao.com>
2020-07-16 16:31:13 +0800

    添加后台管理的前端工程

---
+++ web/pages/quan/index.post.data.js
@@ -0,0 +1,108 @@
+export default [{
+		"post_id": '1',
+		"uid": 1,
+		"username": "张三丰",
+		"header_image": "/static/quan/test/header03.jpg",
+		"content": {
+			"text": "好久没来宗祠这边啦，今天特意过来的！！",
+			"images": ["/static/temp/banner31.jpg"]
+		},
+		"islike": 0,
+		"like": [{
+				"uid": 2,
+				"username": "张翠山,"
+			},
+			{
+				"uid": 3,
+				"username": "张翠松"
+			}
+		],
+		"comments": {
+			"total": 2,
+			"comment": [{
+					"uid": 2,
+					"username": '灭绝',
+					"content": "老哥，贫尼也在此地，要不要聚聚"
+				},
+				{
+					"uid": 3,
+					"username": '张无忌',
+					"content": "一起去好吗?"
+				}
+			]
+		},
+		"timestamp": "5分钟前"
+	},
+	{
+		"post_id": 2,
+		"uid": 1,
+		"username": "张无忌",
+		"header_image": "/static/quan/test/header04.jpg",
+		"content": {
+			"text": "赵敏生了，男孩！",
+			"images": []
+		},
+		"islike": 0,
+		"like": [{
+				"uid": 2,
+				"username": "老王,"
+			},
+			{
+				"uid": 3,
+				"username": "小昭"
+			}
+		],
+		"comments": {
+			"total": 2,
+			"comment": [{
+					"uid": 2,
+					"username": '周芷若',
+					"content": "祝福！！！！！！"
+				},
+				{
+					"uid": 3,
+					"username": '小昭',
+					"content": "祝福！！！！！！!"
+				}
+			]
+		},
+		"timestamp": "1小时前"
+	},
+	{
+		"post_id": 2,
+		"uid": 1,
+		"username": "周芷若",
+		"header_image": "/static/quan/test/header05.jpg",
+		"content": {
+			"text": "清明祭祖求同行～",
+			"images": ["/static/quan/test/header01.jpg", "/static/quan/test/header01.jpg",
+				"/static/quan/test/header01.jpg", "/static/quan/test/header01.jpg"
+			]
+		},
+		"islike": 0,
+		"like": [{
+				"uid": 2,
+				"username": "小王子,"
+			},
+			{
+				"uid": 3,
+				"username": "张大大"
+			}
+		],
+		"comments": {
+			"total": 2,
+			"comment": [{
+					"uid": 2,
+					"username": '小虎',
+					"content": "来了来了!"
+				},
+				{
+					"uid": 3,
+					"username": '小狼',
+					"content": "霍霍霍霍霍霍霍霍霍霍霍霍霍霍霍霍霍霍霍霍霍霍!"
+				}
+			]
+		},
+		"timestamp": "7小时前"
+	}
+]
