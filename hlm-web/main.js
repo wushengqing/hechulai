96102761bb85aa1e98b343af838266ffcea28ea3
sqwu <sqwu@yuanqubao.com>
2020-12-03 23:18:57 +0800

    修改bug及微信分享

--- hlm-web/main.js
+++ hlm-web/main.js
@@ -6,6 +6,7 @@ import Json from './Json' //测试用数据
 
 import request from './request' //测试用数据
 
+require('./utils/wxShare');
 /**
  *  因工具函数属于公司资产, 所以直接在Vue实例挂载几个常用的函数
  *  所有测试用数据均存放于根目录json.js
--- web/utils/wxShare.js
+++ web/utils/wxShare.js
@@ -2,35 +2,38 @@ const baseUrl = 'https://www.hclzz.com/hcl-web/web/'
 var wx = require('./jweixin-module')
 		//获取微信公众号的配置
 		uni.request({
-			url:baseUrl+'getconfig',
+			url:baseUrl+'getconfig.json',
 			dataType: 'text',
 			method: 'POST',
 			data: {
-				url: 'https://www.hclzz.com/web/#/'
+				url:location.origin + '/web/'
 			},
 			success: res => {
+					 data = JSON.parse(res.data);
 					 wx.config({
 						 debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-						 appId: res.data.appId, // 必填，公众号的唯一标识
-						 timestamp: res.data.timestamp, // 必填，生成签名的时间戳
-						 nonceStr: res.data.nonceStr, // 必填，生成签名的随机串
-						 signature: res.data.signature.toLowerCase(), // 必填，签名，见附录1
-						 jsApiList: []
+						 appId: data.appId, // 必填，公众号的唯一标识
+						 timestamp: data.timestamp, // 必填，生成签名的时间戳
+						 nonceStr: data.nonceStr, // 必填，生成签名的随机串
+						 signature: data.signature.toLowerCase(), // 必填，签名，见附录1
+						 jsApiList: ['onMenuShareAppMessage',]
 					});	
 					
 					let shareVo = {
 						title:'何处来族谱',
-						desc:'全球第一部在线族谱',
-						link:'https://www.hclzz.com/web/',
-						imgUrl:'',
+						link:'https://www.hclzz.com/web',
+						imgUrl:'http://photo-project-jcy.oss-cn-shenzhen.aliyuncs.com/hcl/1f939839-5f58-41b2-8f3b-f8c77c6f9cd5.jpg',
 						success(){
 							
 						}
 					}
 					
 					wx.ready(function () {
-						wx.updateAppMessageShareData(shareVo);
-						wx.updateTimelineShareData(shareVo)
+						wx.onMenuShareAppMessage({
+							...shareVo,
+							desc:'传承宗族文化，发扬民族精神',
+						});
+						//wx.updateTimelineShareData(shareVo)
 					});
 			},
 			fail: err => {
